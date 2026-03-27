84343d2de18df475bf91012b28a4a2615ec9af99
abecker-83-beep <abecker-83@hotmail.com>
2026-03-27 14:18:59 -0300

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,1 @@
-SHEET_URL = "SHEET_URL = "https://docs.google.com/spreadsheets/d/12irFPpgWBUKhpjvJNMabfVSomGOD2sZhriwNfklyeZ4/export?format=csv""
+SHEET_URL = "https://docs.google.com/spreadsheets/d/12irFPpgWBUKhpjvJNMabfVSomGOD2sZhriwNfklyeZ4/export?format=csv"
